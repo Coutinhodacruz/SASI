167de31531ce0ca762f73f0a7823b1a0faa595ea
onukwufor dominic <dominicrotimi@gmail.com>
2023-12-26 21:20:09 -0500

    Done with the static and the contact page

--- src/view/component/donate/Donate.jsx
+++ src/view/component/donate/Donate.jsx
@@ -1,10 +1,72 @@
-import React from 'react'
+import React from "react";
+import hand from "../../../assets/image/hand.png";
+import help from "../../../assets/image/help.jpg";
+import DonateStyle from "../../../styles/donate/DonateStyle.css";
+import { FaWallet, FaUser, FaGift, FaCamera } from "react-icons/fa";
 
 const Donate = () => {
-  return (
-    <div>Donate</div>
-    
-  )
-}
+  const iconStyle = { color: "#777", opacity: 0.4 };
 
-export default Donate
+  return (
+    <section className="donate">
+      <div className="container">
+        <div className="section-title">
+          <h2>How Can You Help</h2>
+          <div className="icon-img">
+            <img src={hand} alt="" />
+          </div>
+          <p className="p">
+            There are many variations of azer duskam of Lorem Ipsum available,
+          </p>
+        </div>
+        <div className="col-help">
+          <div className="help-banner">
+            <img src={help} alt="" />
+          </div>
+        </div>
+        <div className="column1">
+          <div className="single-service">
+            <FaWallet size={40} style={iconStyle} />
+            <h3>Send Donation </h3>
+            <p>
+              There are many variations of passages <br /> of Lorem Ipsum
+              available,
+            </p>
+          </div>
+        </div>
+        <div className="column2">
+          <div className="single-service">
+            <FaUser size={40} style={iconStyle} />
+            <h3>Become Volunteer</h3>
+            <p>
+              There are many variations of passages <br /> of Lorem Ipsum
+              available,
+            </p>
+          </div>
+        </div>
+        <div className="column-sec1">
+          <div className="single-service">
+            <FaGift size={40} style={iconStyle} />
+            <h3>Make A Gift</h3>
+            <p>
+              There are many variations of passages <br /> of Lorem Ipsum
+              available,
+            </p>
+          </div>
+        </div>
+        <div className="column-sec2">
+          <div className="single-service">
+            <FaCamera size={40} style={iconStyle} />
+            <h3>Through Media</h3>
+            <p>
+              There are many variations of passages <br /> of Lorem Ipsum
+              available,
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Donate;
--- src/view/component/homepage/HomPage.jsx
+++ src/view/component/homepage/HomPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Link } from 'react-scroll'
+import { Link, animateScroll as scroll } from 'react-scroll';
 import HomePage from "../../../styles/homepage/HomePage.css";
 import sasi from "../../../assets/image/sasi.jpg";
+
 
 const HomPage = () => {
   return (
       <section className="home">
         <header className="header">
+          {/* <div class="bx bx-menu" id="menu-icon"><span class="animate" style="--i:2;"></span></div> */}
         <div className="logo">
           <Link to="home" spy={true} smooth={true} duration={300}>
             <img src={sasi} alt="Logo" />
