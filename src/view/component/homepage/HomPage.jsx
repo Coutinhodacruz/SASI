855534d0fd65e86d417cfe5932d0b74a23d7b1c8
Estheraiyeola <estheraiyeola@yahoo.com>
2024-01-11 23:18:41 +0100

    homepage added

--- src/view/component/homepage/HomPage.jsx
+++ src/view/component/homepage/HomPage.jsx
@@ -1,66 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import "../../../styles/homepage/HomePage.css";
-import { Link } from "react-scroll";
+import { Link, animateScroll as scroll } from 'react-scroll';
 import sasi from "../../../assets/image/sasi.jpg";
 
 const HomPage = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <section className="home">
-      <header className={`header ${isMenuOpen ? "mobile-menu-open" : ""}`}>
-        <div className="home-logo">
+      <header className="header">
+        <div className="bx bx-menu" id="menu-icon">
+          <span className="animate" style={{ "--i": 2 }}></span>
+        </div>
+        <div className="logo">
           <Link to="home" spy={true} smooth={true} duration={300}>
             <img src={sasi} alt="Logo" />
           </Link>
         </div>
         <div className="main">
-          <nav id="navbar" className={isMenuOpen ? "open" : ""}>
-            <Link
-              to="home"
-              spy={true}
-              smooth={true}
-              duration={300}
-              className="active"
-              onClick={toggleMenu}
-            >
+          <nav className="navbar">
+            <Link to="home" spy={true} smooth={true} duration={300} className="active">
               Home
             </Link>
-            <Link
-              to="about"
-              spy={true}
-              smooth={true}
-              duration={300}
-              onClick={toggleMenu}
-            >
+            <Link to="about" spy={true} smooth={true} duration={300}>
               About
             </Link>
-            <Link
-              to="event"
-              spy={true}
-              smooth={true}
-              duration={500}
-              onClick={toggleMenu}
-            >
+            <Link to="event" spy={true} smooth={true} duration={500}>
               Event
             </Link>
-            <Link
-              to="contact"
-              spy={true}
-              smooth={true}
-              duration={500}
-              onClick={toggleMenu}
-            >
+            <Link to="contact" spy={true} smooth={true} duration={500}>
               Contact
             </Link>
           </nav>
-          <div className="nav-btn" onClick={toggleMenu} style={{ color: "white" }}>
-            ☰
-          </div>
         </div>
       </header>
       <div className="content">
