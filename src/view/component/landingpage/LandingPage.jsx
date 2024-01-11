97e25dcc090f56b5fb8ca6913cf424ef9c0e3421
Estheraiyeola <estheraiyeola@yahoo.com>
2024-01-11 22:45:04 +0100

    Newer update

--- src/view/component/landingpage/LandingPage.jsx
+++ src/view/component/landingpage/LandingPage.jsx
@@ -51,16 +51,16 @@ const LandingPage = () => {
           <a href='#hero-section'><img src={Joshiverse} alt='Icon' style={{height: "90px",width: '80px', borderRadius: '10px'}}/></a>
         </div>
         <div id='nav' className={isMenuOpen ? 'open' : ''}>
-          <a href='#about' onClick={toggleMenu}>
+          <a className='nav-a' href='#about' onClick={toggleMenu}>
             About
           </a>
-          <a href='#gallery' onClick={toggleMenu}>
+          <a className='nav-a'href='#gallery' onClick={toggleMenu}>
             Gallery
           </a>
-          <a href='#sasi-intro' onClick={toggleMenu}>
+          <a className='nav-a' href='#sasi-intro' onClick={toggleMenu}>
             SASI
           </a>
-          <a href='#footer' onClick={toggleMenu}>
+          <a className='nav-a' href='#footer' onClick={toggleMenu}>
             Contact
           </a>
         </div>
@@ -74,7 +74,7 @@ const LandingPage = () => {
             <a href='#sasi-intro'>SASI</a>
             <a href='#footer'>Contact</a>
           </div>
-)}
+        )}
         
       </div>
       <div id='hero-section'>
@@ -83,7 +83,7 @@ const LandingPage = () => {
         <p>There are many variations of passages of Lorem Ipsum available,</p> 
         <p>but the majority have suffered alteration in some form.</p>
       </div>
-      <ScrollButton targetIds={['about', 'gallery', 'footer']} />
+      <ScrollButton className='scroll' targetIds={['about', 'gallery', 'footer']} />
       <div id='about'>
         <div id='about-heading'>
           <h2 className='headings'>About Joshiverse</h2>
@@ -94,14 +94,14 @@ const LandingPage = () => {
           <div>
             <div className='vision-mission-header'>
               <p className='clicked-text-head'>Our Mission</p>
-              <p>There are many variations of passages of Lorem Ipsum available, <br/>
+              <p className='p'>There are many variations of passages of Lorem Ipsum available, <br/>
                   There are many variations of passages of Lorem Ipsum available,
                   many variations passages of Lorem available, 
                   and they try to<br/> hard work.</p>
             </div>
             <div className='vision-mission-header'>
               <p className='clicked-text-head'>Our Vision</p>
-              <p>There are many variations of passages of Lorem Ipsum available, <br/>
+              <p className='p'>There are many variations of passages of Lorem Ipsum available, <br/>
                   There are many variations of passages of Lorem Ipsum available,
                   many variations passages of Lorem available, 
                   and they try to<br/> hard work.</p>
@@ -112,7 +112,7 @@ const LandingPage = () => {
 
         </div>
       </div>
-      <ScrollButton targetIds={['about', 'gallery', 'footer']} />
+      <ScrollButton className='scroll' targetIds={['about', 'gallery', 'footer']} />
       <div id='gallery'>
         <div id='gallery-first'>
           <h2 className='headings'>Joshiverse Gallery</h2>
@@ -179,22 +179,20 @@ const LandingPage = () => {
           </Slider>
         </div>
       </div>
-      <ScrollButton targetIds={['about', 'gallery', 'footer', 'sasi-intro']} />
+      <ScrollButton className='scroll' targetIds={['about', 'gallery', 'footer', 'sasi-intro']} />
       <div id='sasi-intro'>
-        <div id='text-sasi'>
-          <div id='inner-sasi'>
-            <h2 className='headings'>SA<span>SI</span></h2>
-            <img src={ImageIcon} alt=''/>
-          </div>
-          <p>It is a long established fact that a reader will be distracted by the readable content of<br/> a page when looking at its layout. 
-          The point of using Lorem Ipsum is that it has a<br/> more-or-less normal distribution of letters, as opposed</p>
-
-          <Link to='/sasi'>
-            <button id='sasi-button'>Read More...</button>
-          </Link>
+        <div id='inner-sasi'>
+          <h2 className='headings'>SA<span>SI</span></h2>
+          <img src={ImageIcon} alt=''/>
         </div>
+        <p>It is a long established fact that a reader will be distracted by the readable content of<br/> a page when looking at its layout. 
+        The point of using Lorem Ipsum is that it has a<br/> more-or-less normal distribution of letters, as opposed</p>
+
+        <Link to='/sasi'>
+          <button id='sasi-button'>Read More...</button>
+        </Link>
       </div>
-      <ScrollButton targetIds={['about', 'gallery', 'footer']} />
+      <ScrollButton className='scroll' targetIds={['about', 'gallery', 'footer']} />
       <div id='footer'>
         <div id='first-footer'>
           <div id='first-column-footer'>
@@ -212,23 +210,29 @@ const LandingPage = () => {
           </div>
           <div id='second-column-footer'>
             <h4>IMPORTANT INFO</h4>
-            <a href='#about'>About Joshiverse</a>
-            <a href='#nav'>Annual Reports</a>
-            <a href='#nav'>Mission & Vision</a>
-            <a href='#nav'>Statistics</a>
+              <div>
+                <a href='#about'>About Joshiverse</a>
+                <a href='#nav'>Annual Reports</a>
+                <a href='#nav'>Mission & Vision</a>
+                <a href='#nav'>Statistics</a>
+              </div>
+            
 
           </div>
           <div id='third-column-footer'>
             <h4>IMPORTANT LINKS</h4>
-            <a href='#nav'>Donate</a>
-            <a href='#gallery'>Media Resources</a>
-            <a href='#nav'>Quick Contact</a>
-            <a href='#nav'>Address</a>
+            <div>
+              <a href='#nav'>Donate</a>
+              <a href='#gallery'>Media Resources</a>
+              <a href='#nav'>Quick Contact</a>
+              <a href='#nav'>Address</a>
+            </div>
+            
           </div>
 
         </div>
         <div id='second-footer'>
-          <hr></hr>
+          <hr ></hr>
           <p>Copyright © Trust {year()} . ALL Right Reserved .</p>
         </div>
 
